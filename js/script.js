89472f02ca32e6d2afd2b97a97e61313eca32c81
IceEmblem <aaksentis.aa@gmail.com>
2026-05-10 19:37:49 +0300

    Fixed indentation in JS file

--- js/script.js
+++ js/script.js
@@ -4,24 +4,23 @@ const errorMessage = document.getElementById("email-error");
 const article = document.getElementById("article");
 
 if (form && emailInput && errorMessage && article) {
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-    if(emailInput.validity.valueMissing) {
-        errorMessage.textContent = "Email is required";
-        emailInput.classList.add("error");
-    }
-    else if(emailInput.validity.typeMismatch) {
-        errorMessage.textContent = "Valid email required";
-        emailInput.classList.add("error");
-    }
-    else {
-        errorMessage.textContent = "";
-        emailInput.classList.remove("error");
-        const userEmail = document.getElementById("user-email");
-        userEmail.textContent = emailInput.value;
-        article.classList.add("success");
-    }
-    
+        if(emailInput.validity.valueMissing) {
+            errorMessage.textContent = "Email is required";
+            emailInput.classList.add("error");
+        }
+        else if(emailInput.validity.typeMismatch) {
+            errorMessage.textContent = "Valid email required";
+            emailInput.classList.add("error");
+        }
+        else {
+            errorMessage.textContent = "";
+            emailInput.classList.remove("error");
+            const userEmail = document.getElementById("user-email");
+            userEmail.textContent = emailInput.value;
+            article.classList.add("success");
+        }
 });
 }
