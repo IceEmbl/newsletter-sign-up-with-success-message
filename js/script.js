eb229a3c98a7c4bc261b82a1f77ce15155f01708
IceEmblem <aaksentis.aa@gmail.com>
2026-05-10 19:30:53 +0300

    Project done!

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("form");
+const emailInput = document.getElementById("email");
+const errorMessage = document.getElementById("email-error");
+const article = document.getElementById("article");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if(emailInput.validity.valueMissing) {
+        errorMessage.textContent = "Email is required";
+        emailInput.classList.add("error");
+    }
+    else if(emailInput.validity.typeMismatch) {
+        errorMessage.textContent = "Valid email required";
+        emailInput.classList.add("error");
+    }
+    else {
+        errorMessage.textContent = "";
+        emailInput.classList.remove("error");
+        const userEmail = document.getElementById("user-email");
+        userEmail.textContent = emailInput.value;
+        article.classList.add("success");
+    }
+    
+});
